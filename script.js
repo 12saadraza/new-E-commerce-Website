f2ee3e0dcc0973eb799a5f9f66ae30f153e91bbe
12saadraza <159704267+12saadraza@users.noreply.github.com>
2024-10-27 17:27:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript for Contact Form Animation
+window.addEventListener('DOMContentLoaded', function() {
+    var contactForm = document.querySelector('.contact-form');
+    setTimeout(function() {
+        contactForm.classList.add('show');
+    }, 500); // Adjust the delay as needed
+});
